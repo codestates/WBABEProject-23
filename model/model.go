10da944009b25905fe5c14e3867bb1382f366333
mihnhyuk <mihnhyuk@naver.com>
2022-12-25 05:00:07 +0900

    [Refactoring] db 이름도 config 파일에서 가져오는게 좋은가?

--- model/model.go
+++ model/model.go
@@ -32,7 +32,7 @@ func NewModel(config *config.Config) (*Model, error) {
 	} else if err := r.client.Ping(context.Background(), nil); err != nil {
 		return nil, err
 	} else {
-		db := r.client.Database("WBA_online_ordering")
+		db := r.client.Database(fmt.Sprintf("%v", config.DB["db"]))
 		r.colBusiness = db.Collection("business")
 		r.colUser = db.Collection("user")
 		r.colOrder = db.Collection("order")
